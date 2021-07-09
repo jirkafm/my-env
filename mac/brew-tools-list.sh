d7bfb2f395f5c40a92bba17e1be93729e922044e
Jiri Kaplan <jiri.kaplan@email.cz>
2021-07-09 15:36:33 +0200

    MacOS configuration

---
+++ mac/brew-tools-list.sh
@@ -0,0 +1,40 @@
+# Casks
+brew install --cask docker
+brew install --cask intellij-idea
+brew install --cask iterm2
+brew install --cask keepaasxc
+brew install --cask microsoft-remote-desktop
+brew install --cask time-out
+# brew install --cask visual-studio-code
+
+# Gui apps
+#brew install macvim
+brew install cherrytree
+brew install cyberduck
+brew install postman
+brew install qutebrowser
+brew install vlc
+
+# Cmd line apps
+brew install cloc
+brew install cmake
+brew install dos2unix
+brew install groovysdk
+brew install htop
+brew install instantclient-basic
+brew install instantclient-sqlplus
+brew install jenv
+brew install jq
+brew install mc
+brew install mutt
+brew install oci-cli
+brew install opensc
+brew install rclone
+brew install rsync
+brew install shellcheck
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install vim
+brew install wget
+brew install --ignore-dependencies maven
